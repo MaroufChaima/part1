65bb52fe785d45294d0ef45d7a4704533449137b
chaima <marouf.chaima.00@gmail.com>
2025-12-23 14:32:42 +0100

    finished exo 1.10

--- unicafe/src/App.jsx
+++ unicafe/src/App.jsx
@@ -4,8 +4,8 @@ const Button = ({onClick, text}) => (
   <button onClick={onClick}>{text}</button> 
 )
 
-const Display = ({ text, value }) => (
-  <p>{text} {value}</p>
+const StatisticLine = ({ text, value }) => (
+  <p>{text}  {value}</p>
 )
 
 
@@ -15,12 +15,12 @@ const Statistics = ({good, neutral, bad, all, average, positive}) => {
   }
   return (
     <div>
-    <Display text= "good " value={good} /> 
-    <Display text="neutral " value={neutral} />
-    <Display text="bad " value={bad} />
-    <Display text="all " value={all} />
-    <Display text="average " value={average} />
-    <Display text="positive " value={positive + ' %'} />
+    <StatisticLine text= "good" value={good} /> 
+    <StatisticLine text="neutral" value={neutral} />
+    <StatisticLine text="bad" value={bad} />
+    <StatisticLine text="all" value={all} />
+    <StatisticLine text="average" value={average} />
+    <StatisticLine text="positive" value={`${positive} %`} />
     </div>
   )
 }
