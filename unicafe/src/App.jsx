d15ad9d921ea35f7bbe036921680ebf762a662b3
chaima <marouf.chaima.00@gmail.com>
2025-12-23 13:34:15 +0100

    finished exo 1.7

--- unicafe/src/App.jsx
+++ unicafe/src/App.jsx
@@ -14,19 +14,26 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const all =  good + neutral + bad
+  const average = all === 0 ? 0 :(good - bad) / all
+  const positive = all === 0 ? 0 :(good / all) * 100
+  
+
 
   return (
     <div>
       <h1>give feedback</h1>
-      <Button text="good" onClick={() => setGood(good + 1)} />
-      <Button text="neutral" onClick={() => setNeutral(neutral + 1)} />
-      <Button text="bad" onClick={() => setBad(bad + 1)} /> 
+      <Button text="good" onClick={()=>{setGood(good +1)}} />
+      <Button text="neutral" onClick={()=>{setNeutral(neutral +1)}} />
+      <Button text="bad" onClick={()=>{setBad(bad +1)}} /> 
         
         <h1>statistics</h1>
         <Display text="good" value={good} />
         <Display text="neutral" value={neutral} />
         <Display text="bad" value={bad} />
-
+        <Display text="all" value={all} />
+        <Display text="average" value={average} />
+        <Display text="positive" value={positive} />
     </div>
   )
 }
